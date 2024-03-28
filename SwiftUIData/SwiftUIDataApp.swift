a651c8780a1754cac4ccacea6e235ba9408c48aa
Razvan Litianu <litianu.razvan@gmail.com>
2024-03-28 13:50:42 +0200

    Initial Commit

---
+++ SwiftUIData/SwiftUIDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDataApp.swift
+//  SwiftUIData
+//
+//  Created by Litianu, Razvan (Cognizant) on 28.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
